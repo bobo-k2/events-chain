23b44b63d512f9f8d9f3fad5fe1f4820a87e3b03
Slobodan Kovacevic <slobodan.kovacevic@8sigma.eu>
2021-06-04 09:14:44 +0200

    Move server to root because of Heroku deployment

--- src/routes/ui.ts
+++ src/routes/ui.ts
@@ -2,12 +2,13 @@
 import express, { Request, Response } from 'express';
 const path = require('path');
 const router = express.Router();
+const clientPath = '../../client/build';
 
 // Have Node serve the files for our built React app
-router.use(express.static(path.resolve(__dirname, '../../../client/build')));
+router.use(express.static(path.resolve(__dirname, clientPath)));
 
 router.get('*', (req: Request, res:Response) => {
-  res.sendFile(path.resolve(__dirname, '../../../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, clientPath, 'index.html'));
 });
 
 export { router as uiRouter };
